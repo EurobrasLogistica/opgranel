0531e25d13f0cc4111cbbc61986b964a1b99c6c5
EurobrasLogistica <132463402+EurobrasLogistica@users.noreply.github.com>
2025-10-02 11:59:18 -0300

    12

--- server/index.js
+++ server/index.js
@@ -2159,72 +2159,88 @@ app.get('/hora/autos/:id', (req, res) => {
   const { id } = req.params;
 
   const sql = `
+  SELECT
+    T.HORA,
+    SUM(T.QUANTIDADE_AUTOS) AS QUANTIDADE_AUTOS
+FROM (
+    /* Autos efetivamente carregados por hora dentro do período aberto */
     SELECT
-      T.HORA,
-      SUM(T.QUANTIDADE_AUTOS) AS QUANTIDADE_AUTOS
-    FROM (
-      /* Autos efetivamente carregados por hora dentro do período aberto */
-      SELECT
         CASE
-          WHEN HOUR(CAR.DATA_CARREGAMENTO) = 23 THEN '23:00 à 00:00'
-          WHEN HOUR(CAR.DATA_CARREGAMENTO) = 0  THEN '00:00 à 01:00'
-          ELSE CONCAT(
-            LPAD(HOUR(CAR.DATA_CARREGAMENTO), 2, '0'), ':00 às ',
-            LPAD(HOUR(CAR.DATA_CARREGAMENTO) + 1, 2, '0'), ':00'
-          )
+            WHEN HOUR(CAR.DATA_CARREGAMENTO) = 23 THEN '23:00 à 00:00'
+            WHEN HOUR(CAR.DATA_CARREGAMENTO) = 0  THEN '00:00 à 01:00'
+            ELSE CONCAT(
+                LPAD(HOUR(CAR.DATA_CARREGAMENTO), 2, '0'), ':00 às ',
+                LPAD(HOUR(CAR.DATA_CARREGAMENTO) + 1, 2, '0'), ':00'
+            )
         END AS HORA,
-        COUNT(1) AS QUANTIDADE_AUTOS,
-        HOR.ORDEM AS ORDEM
-      FROM CARREGAMENTO CAR
-      INNER JOIN VW_HORARIOS_2 HOR
+        COUNT(*) AS QUANTIDADE_AUTOS,
+        HOR.ORDEM
+    FROM CARREGAMENTO CAR
+    INNER JOIN VW_HORARIOS_2 HOR
         ON HOR.HORA = HOUR(CAR.DATA_CARREGAMENTO)
-      WHERE CAR.STATUS_CARREG = 3
-        AND CAR.PESO_BRUTO > 0
-        AND CAR.COD_OPERACAO = ?
-        AND CAR.DATA_CARREGAMENTO >= (
-          SELECT MAX(PO.DAT_INI_PERIODO)
+    WHERE CAR.STATUS_CARREG = 3
+      AND CAR.PESO_BRUTO > 0
+      AND CAR.COD_OPERACAO = ?
+      AND CAR.DATA_CARREGAMENTO >= (
+          SELECT PO.DAT_INI_PERIODO
           FROM PERIODO_OPERACAO PO
           WHERE PO.COD_OPERACAO = CAR.COD_OPERACAO
             AND PO.DAT_FIM_PERIODO IS NULL
-        )
-        AND CAR.DATA_CARREGAMENTO <= (
+          ORDER BY PO.DAT_INI_PERIODO DESC
+          LIMIT 1
+      )
+      AND CAR.DATA_CARREGAMENTO <= (
           SELECT CASE
-                   WHEN TIME_FORMAT(PO.DAT_INI_PERIODO, '%H:%i:%s')
-                        BETWEEN '19:00:00' AND '23:59:59'
-                   THEN CONCAT(
-                          DATE_FORMAT(MAX(DATE_ADD(PO.DAT_INI_PERIODO, INTERVAL 1 DAY)), '%Y-%m-%d'),
-                          ' ', PE.FIM_PERIODO, ':00'
-                        )
-                   ELSE CONCAT(
-                          DATE_FORMAT(MAX(PO.DAT_INI_PERIODO), '%Y-%m-%d'),
-                          ' ', PE.FIM_PERIODO, ':00'
-                        )
+                    WHEN TIME_FORMAT(PO.DAT_INI_PERIODO, '%H:%i:%s')
+                         BETWEEN '19:00:00' AND '23:59:59'
+                    THEN CONCAT(
+                             DATE_FORMAT(DATE_ADD(PO.DAT_INI_PERIODO, INTERVAL 1 DAY), '%Y-%m-%d'),
+                             ' ', PE.FIM_PERIODO, ':00'
+                         )
+                    ELSE CONCAT(
+                             DATE_FORMAT(PO.DAT_INI_PERIODO, '%Y-%m-%d'),
+                             ' ', PE.FIM_PERIODO, ':00'
+                         )
                  END
           FROM PERIODO_OPERACAO PO
           INNER JOIN PERIODO PE ON PE.COD_PERIODO = PO.COD_PERIODO
           WHERE PO.COD_OPERACAO = CAR.COD_OPERACAO
             AND PO.DAT_FIM_PERIODO IS NULL
-        )
-      GROUP BY HOUR(CAR.DATA_CARREGAMENTO)
+          ORDER BY PO.DAT_INI_PERIODO DESC
+          LIMIT 1
+      )
+    GROUP BY
+        CASE
+            WHEN HOUR(CAR.DATA_CARREGAMENTO) = 23 THEN '23:00 à 00:00'
+            WHEN HOUR(CAR.DATA_CARREGAMENTO) = 0  THEN '00:00 à 01:00'
+            ELSE CONCAT(
+                LPAD(HOUR(CAR.DATA_CARREGAMENTO), 2, '0'), ':00 às ',
+                LPAD(HOUR(CAR.DATA_CARREGAMENTO) + 1, 2, '0'), ':00'
+            )
+        END,
+        HOR.ORDEM
 
-      UNION
+    UNION
 
-      /* Linha base com todos os horários possíveis do período aberto */
-      SELECT
+    /* Linha base com todos os horários possíveis do período aberto */
+    SELECT
         H.HORARIO AS HORA,
         H.QUANTIDADE_AUTOS,
         H.ORDEM
-      FROM VW_HORARIOS_2 H
-      WHERE H.INI_PERIODO = (
+    FROM VW_HORARIOS_2 H
+    WHERE H.INI_PERIODO = (
         SELECT HOUR(PO.DAT_INI_PERIODO)
         FROM PERIODO_OPERACAO PO
         INNER JOIN PERIODO PE ON PE.COD_PERIODO = PO.COD_PERIODO
         WHERE PO.COD_OPERACAO = ?
           AND PO.DAT_FIM_PERIODO IS NULL
-      )
-    ) T
-    GROUP BY T.HORA, T.ORDEM
-    ORDER BY T.ORDEM;
+        ORDER BY PO.DAT_INI_PERIODO DESC
+        LIMIT 1
+    )
+) T
+GROUP BY T.HORA, T.ORDEM
+ORDER BY T.ORDEM;
+
   `;
 
   db.query(sql, [id, id], (err, rows) => {
