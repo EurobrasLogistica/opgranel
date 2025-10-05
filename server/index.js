d7a76f050dc891c51a1cf78cac3aad5b7fcedef8
Lucas Rodrigues <82480669+lrodrigues23@users.noreply.github.com>
2025-10-05 18:46:04 -0300

    d

--- server/index.js
+++ server/index.js
@@ -1,6 +1,4 @@
-﻿require('dotenv').config();
-
-const express = require('express');
+﻿const express = require('express');
 const session = require('express-session');
 const cors = require('cors');
 const app = express();
